5182cdcd9497e3e5bf149c942ff720c96f587bb8
Vishal Patil <believeinthelimitless@gmail.com>
2023-12-21 22:57:30 +0530

    Added caching of url images to disk cache

--- app/src/main/java/com/example/gratitudeassignment/GratitudeApplication.kt
+++ app/src/main/java/com/example/gratitudeassignment/GratitudeApplication.kt
@@ -4,26 +4,16 @@ import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
 import coil.disk.DiskCache
-import coil.memory.MemoryCache
-import coil.request.CachePolicy
 import coil.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GratitudeApplication: Application(), ImageLoaderFactory {
     override fun newImageLoader(): ImageLoader {
-        return ImageLoader(this).newBuilder()
-            .memoryCachePolicy(CachePolicy.ENABLED)
-            .memoryCache {
-                MemoryCache.Builder(this)
-                    .maxSizePercent(0.01)
-                    .strongReferencesEnabled(true)
-                    .build()
-            }
-            .diskCachePolicy(CachePolicy.ENABLED)
+        return ImageLoader.Builder(this)
             .diskCache {
                 DiskCache.Builder()
-                    .maxSizePercent(0.001)
+                    .maxSizeBytes(5 * 1024 * 1024)
                     .directory(cacheDir)
                     .build()
             }
