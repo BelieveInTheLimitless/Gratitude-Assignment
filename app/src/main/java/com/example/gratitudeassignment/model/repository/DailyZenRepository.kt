31adbfe9c4106a45a98c34eb4d68a26f5af6d762
Vishal Patil <believeinthelimitless@gmail.com>
2023-12-24 00:06:27 +0530

    Added functions to perform operations on Zen Item entity

--- app/src/main/java/com/example/gratitudeassignment/model/repository/DailyZenRepository.kt
+++ app/src/main/java/com/example/gratitudeassignment/model/repository/DailyZenRepository.kt
@@ -2,11 +2,14 @@ package com.example.gratitudeassignment.model.repository
 
 import android.util.Log
 import com.example.gratitudeassignment.model.data.DataOrException
+import com.example.gratitudeassignment.model.data.dao.DailyZenItemDao
+import com.example.gratitudeassignment.model.data.local.DailyZenItemEntity
 import com.example.gratitudeassignment.model.data.remote.DailyZen
 import com.example.gratitudeassignment.model.data.remote.DailyZenApi
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class DailyZenRepository @Inject constructor(private val api: DailyZenApi) {
+class DailyZenRepository @Inject constructor(private val api: DailyZenApi, private val dailyZenItemDao: DailyZenItemDao) {
     suspend fun getDailyZen(date: String): DataOrException<DailyZen, Boolean, Exception> {
         val response = try {
             api.getDailyZen(date = date)
@@ -18,4 +21,12 @@ class DailyZenRepository @Inject constructor(private val api: DailyZenApi) {
         return DataOrException(data = response)
 
     }
+
+//    Database
+
+    fun getDailyZenItems(date: String): Flow<List<DailyZenItemEntity>> = dailyZenItemDao.getDailyZenItems(date = date)
+
+    suspend fun insertDailyZenItem(dailyZenItemEntity: DailyZenItemEntity) = dailyZenItemDao.insertDailyZenItem(dailyZenItemEntity = dailyZenItemEntity)
+
+    suspend fun deleteAllDailyZen() = dailyZenItemDao.deleteAllDailyZen()
 }
