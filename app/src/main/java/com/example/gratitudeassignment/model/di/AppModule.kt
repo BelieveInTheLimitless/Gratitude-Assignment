007daff90af2de8b725dcc2c7b88d50b20d843a2
Vishal Patil <believeinthelimitless@gmail.com>
2023-12-24 00:07:15 +0530

    Added Database provider in AppModule

--- app/src/main/java/com/example/gratitudeassignment/model/di/AppModule.kt
+++ app/src/main/java/com/example/gratitudeassignment/model/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.gratitudeassignment.model.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.gratitudeassignment.model.data.dao.DailyZenItemDao
+import com.example.gratitudeassignment.model.data.dao.DailyZenItemDatabase
 import com.example.gratitudeassignment.model.data.remote.DailyZenApi
 import com.example.gratitudeassignment.model.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -22,4 +27,19 @@ class AppModule {
             .build()
             .create(DailyZenApi::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun provideDailyZeItemDatabase(dailyZenItemDatabase: DailyZenItemDatabase): DailyZenItemDao =
+        dailyZenItemDatabase.dailyZenItemDao()
+
+    @Singleton
+    @Provides
+    fun provideDailyZenItemDatabase(@ApplicationContext context: Context): DailyZenItemDatabase =
+        Room.databaseBuilder(
+            context,
+            DailyZenItemDatabase::class.java,
+            "daily_zen_item_database"
+        ).fallbackToDestructiveMigration()
+            .build()
 }
